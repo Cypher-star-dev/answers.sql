cc90eecd24bfce5a670a16b3e7ed59955338f0ce
Cypher-star-dev <omogagikonyo31@gmail.com>
2025-03-20 10:28:02 +0300

    Update answes.sql

--- answes.sql
+++ answes.sql
@@ -1,3 +1,3 @@
 Questions 1. The SQL query to create a new database is CREATE DATABASE salesDB;   That command creates a new databse called salesDB.
-Question 2. The SQL query to create a new database for demo is CREATE DATABASE demo: That command creates a new database called salesDB.
+Question 2. The SQL query to create a new database for demo is CREATE DATABASE demo: That command creates a new database called demo.
 Question 2. The SQL query to drop (delete) is  DROP DATABASE demo;   That command deletes the database and all its contents. First you have to create the Demo database before dropping or deleting it.
